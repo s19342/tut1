518127998af1a0f87949d0062241ba504a44bacf
AdnanAzam <s19342@pjwstk.edu.pl>
2020-03-11 10:08:33 +0100

    commit

--- Tutorial1_Solution/Tutorial1/Program.cs
+++ Tutorial1_Solution/Tutorial1/Program.cs
@@ -9,10 +9,19 @@ namespace Tutorial1
     {
         public static async Task Main(string[] args)
         {
+            if (args.Length == 0) 
+            {
+                throw new ArgumentNullException("Need to pass an argument");
+            }
+            if (args[0] == null)
+            {
+                throw new ArgumentException("No website url argument");
+            }
+
             var websiteUrl = args[0];
             var httpClient = new HttpClient();
             var response = await httpClient.GetAsync(websiteUrl);
-            if(response.IsSuccessStatusCode)
+            if (response.IsSuccessStatusCode)
             {
                 var htmlContent = await response.Content.ReadAsStringAsync();
 
@@ -20,12 +29,24 @@ namespace Tutorial1
 
                 var matches = regex.Matches(htmlContent);
 
+                if (matches.Count == 0)
+                {
+                    Console.WriteLine("No email addresses found");
+                }
+
                 foreach (var emailAddress in matches)
                 {
                     Console.WriteLine(emailAddress.ToString());
                 }
             }
+            else
+            {
+                Console.WriteLine("Error while downloading the page");
+            }
             Console.ReadKey();
         }
+
+        //need to implement unique only email addresses
+        //need to implement dispose
     }
 }
